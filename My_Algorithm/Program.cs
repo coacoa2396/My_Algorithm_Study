9266e0fcfdd3048572819d9707981d46311155d5
ChanGyu Yu <dbcksrb99@gmail.com>
2024-01-11 09:27:26 +0900

    feat : 네이밍 수정

--- My_Algorithm/Program.cs
+++ My_Algorithm/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace My_Algorithm
+﻿namespace _00_Algorithm
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
