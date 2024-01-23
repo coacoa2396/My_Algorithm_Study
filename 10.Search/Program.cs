6ceb2e0a72acce40784c1fec0bd4a308fa6a8579
ChanGyu Yu <dbcksrb99@gmail.com>
2024-01-23 17:38:10 +0900

    feat : 과제 커밋

---
+++ 10.Search/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _10.Search
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // 순차탐색
+            int[] array = new int[] { 1, 3, 5, 7, 9, 8, 6, 4, 2, 0 };
+
+            int indexOf = Array.IndexOf(array, 11);
+            int result = Searching.SequentialSearch(array, 11);
+            Console.WriteLine($"순차탐색 결과 위치 : {indexOf}");
+            Console.WriteLine($"구현한 결과 위치 : {result}");
+
+
+
+            // 이진탐색
+            Console.WriteLine("정렬 전");
+            int binarySearch;
+            int result2;
+            binarySearch = Array.BinarySearch(array, 2);
+            result2 = Searching.BinarySearch(array, 2);
+            Console.WriteLine($"정렬 전 이진탐색 결과 : {binarySearch}");
+
+            Array.Sort(array);
+
+            Console.WriteLine("정렬 후");
+            binarySearch = Array.BinarySearch(array, 2);
+            result2 = Searching.BinarySearch(array, 2);
+            Console.WriteLine($"정렬 후 이진탐색 결과 : {binarySearch}");
+
+
+
+            bool[,] graph = new bool[8, 8]
+            {
+                { false,  true, false, false, false, false, false, false },
+                {  true, false,  true, false, false,  true, false, false },
+                { false,  true, false, false,  true,  true, false, false },
+                { false, false, false, false, false,  true, false, false },
+                { false, false,  true, false, false, false,  true,  true },
+                { false,  true,  true,  true, false, false, false, false },
+                { false, false, false, false,  true, false, false, false },
+                { false, false, false, false,  true, false, false, false },
+            };
+
+
+            // DFS 탐색
+            Searching.DFS(in graph, 0, out bool[] dfsVisited, out int[] dfsPath);
+            Console.WriteLine("<DFS>");
+            PrintGraphSearch(dfsVisited, dfsPath);
+            Console.WriteLine();
+
+
+            // BFS 탐색
+            Searching.BFS(in graph, 0, out bool[] bfsVisited, out int[] bfsPath);
+            Console.WriteLine("<BFS>");
+            PrintGraphSearch(bfsVisited, bfsPath);
+            Console.WriteLine();
+        }
+
+        private static void PrintGraphSearch(bool[] visited, int[] path)
+        {
+            Console.WriteLine($"{"Vertex",8}{"Visit",8}{"Path",8}");
+
+            for (int i = 0; i < visited.Length; i++)
+            {
+                Console.WriteLine($"{i,8}{visited[i],8}{path[i],8}");
+            }
+        }
+    }
+    }
+}
---
+++ 10.Search/homework.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Search
+{
+    internal class homework
+    {
+        // 깊이 우선 탐색 (Depth-First Search)
+        // 스택을 통해 구현하기
+        public static void DFS(bool[,] graph, int start, out bool[] visited, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for (int i = 0; i < graph.GetLength(0); i++)  // 초기 세팅
+            {
+                visited[i] = false;
+                parents[i] = -1;
+            }
+
+            SearchNode(graph, start, visited, parents);
+        }
+
+        public static void SearchNode(bool[,] graph, int start, bool[] visited, int[] parents)
+        {
+            visited[start] = true;
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (graph[start, i] && !visited[i])
+                {
+                    parents[i] = start;
+                    SearchNode(graph, i, visited, parents);
+                }
+            }
+        }
+
+
+        // 너비 우선 탐색
+        // 큐를 통해 구현하기
+        public static void BFS(in bool[,] graph, int start, out bool[] visited, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                parents[i] = -1;
+            }
+            visited[start] = true;
+
+            Queue<int> bfsQueue = new Queue<int>();
+
+            bfsQueue.Enqueue(start);
+            while (bfsQueue.Count > 0)
+            {
+                int next = bfsQueue.Dequeue();
+
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (graph[next, i] && !visited[i])
+                    {
+                        visited[i] = true;
+                        parents[i] = next;
+                        bfsQueue.Enqueue(i);
+                    }
+                }
+            }
+        }
+    }
+}
